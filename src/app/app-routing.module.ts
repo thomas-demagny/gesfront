cc3c9298882f357d4bdd6c60167f71173df9f0df
Tom <demagny.t@gmail.com>
2022-10-20 09:44:50 +0200

    add address and app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressDetailsComponent } from './address/address-details/address-details.component';
+import { AddressListComponent } from './address/address-list/address-list.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { 
+    path: 'address',
+     
+     children: [ {
+
+      path: 'list',
+      component: AddressListComponent
+    },
+    {
+        path: 'test',
+        component: AddressDetailsComponent,
+      }]
+  }
+    
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressListComponent } from './address/address-list/address-list.component';
+import { AddressDetailsComponent } from './address/address-details/address-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressListComponent,
+    AddressDetailsComponent,
+   
   ],
   imports: [
     BrowserModule,
